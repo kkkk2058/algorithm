c5ebfcbf3f1bc2d233fdcd23832ac2d76b9ea581
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-03-25 23:54:25 +0900

    7569.py 업데이트

--- DFS와 BFS/7569/7569.py
+++ DFS와 BFS/7569/7569.py
@@ -1,3 +1,11 @@
 import sys
 
 n,m,h = map(int,sys.stdin.readline().split())
+
+for i in range(n):
+    a = int(input())
+    for j in range(m):
+        b = int(input())
+        for k in range(h):
+            c = int(input())
+
