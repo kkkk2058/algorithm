a750595640c97113f1dc8309273327825b20a6f9
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-03-29 23:26:46 +0900

    Update input reading and BFS setup
    Refactor input handling and initialize BFS variables.

--- DFS와 BFS/7569/7569.py
+++ DFS와 BFS/7569/7569.py
@@ -1,14 +1,18 @@
 import sys
+from collections import deque
 
-n,m,h = map(int,sys.stdin.readline().split())
-
-
-for i in range(n):
-    a = int(input())
-    for j in range(m):
-        b = int(input())
-        for k in range(h):
-            c = int(input())
+m,n,h = map(int,sys.stdin.readline().split())
 
 
+matrix = [[ list(map( int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h) ]
 
+visited = [[[False] * m for _ in range(n)] for _ in range(h)] 
+
+dx = [0,0,1,-1,0,0]
+dy = [1,-1,0,0,0,0]
+dx = [0,0,0,0,1,-1]
+
+queue = deque()
+
+
+def bfs:
