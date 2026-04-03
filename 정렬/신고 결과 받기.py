6422df509e61c2d5be3fdaeb98b28ca36290a3ba
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-04-03 21:24:51 +0900

    Create 신고 결과 받기.py

---
+++ 정렬/신고 결과 받기.py
@@ -0,0 +1,31 @@
+def solution(id_list, report, k):
+    answer = []
+    answer_dic = {}
+    reported_dic = {}
+    result=[]
+    report = set(report)
+    for user in id_list:
+        answer_dic[user] = 0
+    # 한줄로 딕셔너리 넣기
+    reported_dic = {user: 0 for user in id_list}
+    
+    
+    for i in report:
+        reporter, reported = i.split(" ")
+        reported_dic[reported] +=1
+        
+    print(reported_dic)
+    
+    for i in report:
+        reporter, reported = i.split(" ")
+        if reported_dic[reported] >=k:
+            answer_dic[reporter] +=1
+    
+    print(answer_dic)
+    
+    
+    for k in id_list:
+        # user 를 넣어야되므로 반복문안에 있는 k 를 인자로 넣어주어야 한다
+        result.append(answer_dic[k])
+    answer = result
+    return answer
