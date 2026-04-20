6603e62175c27c9c5362012cabd710420add5b21
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-04-20 23:26:15 +0900

    Update 7569.py

--- DFS와 BFS/7569/7569.py
+++ DFS와 BFS/7569/7569.py
@@ -6,7 +6,7 @@ m,n,h = map(int,sys.stdin.readline().split())
 
 matrix = [[ list(map( int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h) ]
 
-visited = [[[False] * m for _ in range(n)] for _ in range(h)] 
+visited = [[[False] * m for _ in range(n)] for _ in range(h)]  
 
 dx = [0,0,1,-1,0,0]
 dy = [1,-1,0,0,0,0]
