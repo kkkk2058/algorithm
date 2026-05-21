7e5fa44f206b6fc64f9a497927b5b9c1599db71f
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-05-21 23:37:55 +0900

    [Passed] 청소당번 정하기 (49ms, 16MB)

--- trail1/청소당번 정하기/cleaning-numbering.py
+++ trail1/청소당번 정하기/cleaning-numbering.py
@@ -5,12 +5,18 @@ floor = 0
 toilet = 0
 
 for i in range(1,n+1):
-    if i % 12 ==0:
-        toilet += 1
-    if i % 12 !=0 and i % 3 ==0:
+    # if i % 12 ==0:
+    #     toilet += 1
+    # if i % 12 !=0 and i % 3 ==0:
+    #     floor +=1
+    # if i % 12 !=0 and i % 3 !=0 and i% 2 == 0:
+    #     classroom +=1
+    if i % 12 == 0:
+        toilet +=1
+    elif i % 3 == 0:
         floor +=1
-    if i % 12 !=0 and i % 3 !=0 and i% 2 == 0:
-        classroom +=1
+    elif i % 2 == 0:
+        classroom += 1
 
 print(classroom,floor,toilet, end=' ')
         
