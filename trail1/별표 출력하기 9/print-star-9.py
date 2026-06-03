538c0b3b6118a39fe62573db7f7c8290f2c019e2
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-03 23:40:04 +0900

    [Passed] 별표 출력하기 9 (50ms, 16MB)

--- trail1/별표 출력하기 9/print-star-9.py
+++ trail1/별표 출력하기 9/print-star-9.py
@@ -1,9 +1,17 @@
 n = int(input())
 
 
-for i in range(1,2*n+1,2):
-    for _ in range(2*n-1 -i):
-        print(" ",end="")
-    for _ in range(i):
-        print("*" , end= " ")
-    print(" ")
+# for i in range(1,2*n+1,2):
+#     for _ in range(2*n-1 -i):
+#         print(" ",end="")
+#     for _ in range(i):
+#         print("*" , end= " ")
+#     print(" ")
+
+
+for i in range(n):
+    for _ in range(n - i-1):
+        print(" ", end=" ")
+    for _ in range(2*i + 1):
+        print("*", end=" ")
+    print()
