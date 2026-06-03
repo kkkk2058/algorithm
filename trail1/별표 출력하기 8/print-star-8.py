22fd3992204fa632ade46fc35287b5044c09f999
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-04 01:26:24 +0900

    [Passed] 별표 출력하기 8 (47ms, 16MB)

---
+++ trail1/별표 출력하기 8/print-star-8.py
@@ -0,0 +1,20 @@
+# n = int(input())
+
+# for i in range(1,n+1):
+#     if i % 2 == 0:
+#         print("* "*i,end=" ")
+#     else:
+#         print("*",end=" ")
+#     print()
+
+
+
+n = int(input())
+
+for i in range(1,n+1):
+    if i % 2 == 0:
+        for _ in range(i):
+            print("*",end=" ")
+    else:
+        print("*",end=" ")
+    print()
