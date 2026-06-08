7dc62dc10278dcc3868d80b250012d4a4d8ae542
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-09 04:57:29 +0900

    [Passed] 특정 수 도달하기 (48ms, 16MB)

--- trail1/특정 수 도달하기/reaching-specific-number.py
+++ trail1/특정 수 도달하기/reaching-specific-number.py
@@ -6,15 +6,29 @@
 num_list = list(map(int,input().split()))
 
 # print(num_list[0])
-sum = 0
-avg = 0
-for i in range(len(num_list)):
-    if num_list[i] < 250:
-        sum += num_list[i]
-        if i == len(num_list)-1:
-            avg = sum / (i+1)
+# sum = 0
+# avg = 0
+# for i in range(len(num_list)):
+#     if num_list[i] < 250:
+#         sum += num_list[i]
+#         if i == len(num_list)-1:
+#             avg = sum / (i+1)
             
-    else:
-        avg = sum / (i)
+#     else:
+#         avg = sum / (i)
+#         break
+# print(f"{sum} {avg:.1f}")
+
+
+sum = 0
+cnt = 0
+
+for elem in num_list:
+    if elem > 250:
         break
+    sum += elem
+    cnt+=1
+
+avg = sum / cnt
+
 print(f"{sum} {avg:.1f}")
