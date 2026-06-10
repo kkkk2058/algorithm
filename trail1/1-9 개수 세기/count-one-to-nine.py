0ae9bb0da3a8aa29b9d1a2045f3802f4c7442010
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-11 00:18:08 +0900

    [Passed] 1-9 개수 세기 (54ms, 16MB)

---
+++ trail1/1-9 개수 세기/count-one-to-nine.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+count_arr= [0 for _ in range(10)]
+
+arr = list(map(int, input().split()))
+
+for elem in arr:
+    count_arr[elem]+=1
+
+
+for i in range(1,10):
+    print(count_arr[i])
+
