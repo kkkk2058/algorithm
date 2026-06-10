7ab4250768bcf2531e74f65d86d8640a97e5240b
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-10 22:55:21 +0900

    [Passed] 숫자들의 배수 (48ms, 16MB)

--- trail1/숫자들의 배수/multiple-of-numbers.py
+++ trail1/숫자들의 배수/multiple-of-numbers.py
@@ -2,7 +2,19 @@ n = int(input())
 
 cnt = 0
 
-list = []
+# arr = []
+
+# arr.append(n)
+
+# for i in range(1,10):
+#     a = arr[i-1] + arr[0]
+#     arr.append(a)
+
+# for elem in arr:
+#     if elem %5 ==0:
+#         cnt+=1
+#     if cnt>=2:
+#         break
 
 
 for i in range(1,100):
