b90ad3a633e6fda0a63ec8d14bb8eb11257f32ca
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-10 23:41:32 +0900

    [Passed] 100 도달하기 (50ms, 16MB)

---
+++ trail1/100 도달하기/reach-100.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+arr= []
+
+arr.append(1)
+arr.append(n)
+arr.append(n+1)
+# print(arr)
+for i in range(2,100):
+    
+    if arr[i-1] > 100:
+        for num in range(i):
+            print(arr[num],end=" ")
+        break
+    arr.append(arr[i-1] + arr[i])
