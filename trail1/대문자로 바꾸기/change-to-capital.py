0df302ad9150b4596689373b0c1b8c5edef71178
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-06-23 00:23:38 +0900

    [Passed] 대문자로 바꾸기 (49ms, 16MB)

--- trail1/대문자로 바꾸기/change-to-capital.py
+++ trail1/대문자로 바꾸기/change-to-capital.py
@@ -1,7 +1,14 @@
 arr_2d = [list(input().split()) for _ in range(5)]
 
 
-for j in range(5):
-    for i in arr_2d[j]:
-        print(i.upper(),end=" ")
+# for j in range(5):
+#     for i in arr_2d[j]:
+#         print(i.upper(),end=" ")
+#     print()
+
+
+for i in range(5):
+    for j in range(3):
+        arr_2d[i][j] = chr(ord(arr_2d[i][j] )+ ord('A') - ord('a'))
+        print(arr_2d[i][j],end=" ")
     print()
