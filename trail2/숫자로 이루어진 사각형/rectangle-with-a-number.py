bf6cb142edf2847e53d10edafd9100ec6b3b7324
Sehoon.Kim <72534252+kkkk2058@users.noreply.github.com>
2026-07-02 23:11:57 +0900

    [Passed] 숫자로 이루어진 사각형 (54ms, 16MB)

---
+++ trail2/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+
+def square(n):
+    num = 1
+
+
+    for i in range(n):
+        for j in range(n):
+            if num >= 10:
+                num = 1
+            print(num,end=" ")
+            num+=1
+        print()
+
+square(n)
